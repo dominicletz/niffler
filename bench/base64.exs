119fc2f358f8ea04f073125bba3f1195134fab44
Dominic Letz <dominic.letz@berlin.de>
2024-10-08 00:44:22 +0200

    cleanup

--- bench/base64.exs
+++ bench/base64.exs
@@ -8,47 +8,67 @@ defmodule Base64 do
   end
 
   defnif :encode_nif, [input: :binary], ret: :binary do
-    """
-    static char encoding_table[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H',
-    'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
-    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X',
-    'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
-    'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-    'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-    'w', 'x', 'y', 'z', '0', '1', '2', '3',
-    '4', '5', '6', '7', '8', '9', '+', '/'};
-    static int mod_table[] = {0, 2, 1};
+    ~S"""
+    static const unsigned char base64_table[65] =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-    size_t output_length = 4 * (($input.size + 2) / 3);
-    $ret.data = $alloc(output_length);
-    $ret.size = output_length;
-    size_t input_length = $input.size;
-    unsigned char* data = $input.data;
-    char* encoded_data = $ret.data;
-    for (int i = 0; i < $input.size;) {
-      uint32_t octet_a = i < input_length ? data[i++] : 0;
-      uint32_t octet_b = i < input_length ? data[i++] : 0;
-      uint32_t octet_c = i < input_length ? data[i++] : 0;
+      unsigned char *src = $input.data;
+      size_t len = $input.size;
 
-      uint32_t triple = (octet_a << 0x10) + (octet_b << 0x08) + octet_c;
+      unsigned char *out, *pos;
+      const unsigned char *end, *in;
+      size_t olen;
 
-      *encoded_data++ = encoding_table[(triple >> 3 * 6) & 0x3F];
-      *encoded_data++ = encoding_table[(triple >> 2 * 6) & 0x3F];
-      *encoded_data++ = encoding_table[(triple >> 1 * 6) & 0x3F];
-      *encoded_data++ = encoding_table[(triple >> 0 * 6) & 0x3F];
-    }
+      olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
+      olen += olen / 72; /* line feeds */
+      olen++; /* nul termination */
+      if (olen < len)
+        return "olen < len";
+      out = $alloc(olen);
 
-    for (int i = 0; i < mod_table[$input.size % 3]; i++)
-      $ret.data[output_length - 1 - i] = '=';
+      end = src + len;
+      in = src;
+      pos = out;
+      while (end - in >= 3) {
+        *pos++ = base64_table[in[0] >> 2];
+        *pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
+        *pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
+        *pos++ = base64_table[in[2] & 0x3f];
+        in += 3;
+      }
+
+      if (end - in) {
+        *pos++ = base64_table[in[0] >> 2];
+        if (end - in == 1) {
+          *pos++ = base64_table[(in[0] & 0x03) << 4];
+          *pos++ = '=';
+        } else {
+          *pos++ = base64_table[((in[0] & 0x03) << 4) |
+                    (in[1] >> 4)];
+          *pos++ = base64_table[(in[1] & 0x0f) << 2];
+        }
+        *pos++ = '=';
+      }
+
+      *pos = '\0';
+
+      $ret.data = out;
+      $ret.size = pos - out;
     """
   end
 end
 
 # file = "this is a really small string that we are going to encode to base 64"
-file = :rand.bytes(300_000)
+file = :rand.bytes(1024 * 1024)
+
 
 result = Base64.encode(file)
-^result = Base.encode64(file)
+result2 = Base.encode64(file)
+
+if result != result2 do
+  IO.inspect({result, result2})
+  raise "implementations are not matching"
+end
 
 Benchee.run(
   %{
